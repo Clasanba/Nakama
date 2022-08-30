1ec732535ae005af0d983679b793a000d17f80da
Clasanba <clasanba@gmail.com>
2022-08-30 13:27:05 +0000

    Full navbar without styles

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,24 +2,53 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import logoNakama1 from "/workspace/Nakama/src/front/img/Logo_Nakama1.png";
-import "../../styles/home.css";
+import "../../styles/navBar.css";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar bg-success bg-gradient">
-      <div className="container-fluid ">
-        <a className="navbar-brand">
-          <img src={logoNakama1} id="imgnavBar" />
-        </a>
-        <form className="d-flex" role="search">
-          <button class="btn btn-primary" type="button">
-            Registro usuario
-          </button>
-          <button class="btn btn-primary" type="button">
-            Inicio sesión
-          </button>
-        </form>
-      </div>
-    </nav>
+    <>
+      <nav className="navbar bg-success bg-gradient">
+        <div className="container-fluid ">
+          <a className="navbar-brand ">
+            <img src={logoNakama1} className="logo_navBar" />
+          </a>
+          <form className="d-flex" role="search">
+            <button
+              className="bg-success text-white p-2 me-md-3 border-0 bg-opacity-10"
+              type="button"
+            >
+              Registro usuario
+            </button>
+            <button
+              className="bg-success text-white p-2 me-md-3 border-0 bg-opacity-10"
+              type="button"
+            >
+              Inicio sesión
+            </button>
+          </form>
+        </div>
+      </nav>
+      <nav className="navbar bg-success p-2 bg-opacity-50 justify-content-center">
+        <div className=" ">
+          <ul className="nav ">
+            <li className="nav-item ">
+              <a className="nav-link text-white" aria-current="page" href="#">
+                Psicología
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">
+                Nutrición
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">
+                Artículos
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 };
