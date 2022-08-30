cf643efd61490f68e31f0dfe87b8a1d95e21707a
Clasanba <clasanba@gmail.com>
2022-08-30 11:28:58 +0000

    style home fixed

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,17 +6,20 @@ import "../../styles/home.css";
 export const Home = () => {
   return (
     <main className="d-flex justify-content-center  ">
-      <figure className="card border-0" style={{width: 40 + "rem"}}>
-        <img src={logoNakama} className="card-img-top logo_nakama" alt="logo_nakama"/>
+      <figure className="card border-0" style={{ width: 40 + "rem" }}>
+        <img
+          src={logoNakama}
+          className="card-img-top logo_nakamaHome"
+          alt="logo_nakama"
+        />
         <figcaption className="card-body">
           <p className="card-text text-center">
             Somos una comunidad dirigida a personas relacionadas directa o
             indirectamente con el cáncer, a través de la cual, puedan aprender
             aspectos sobre el día a día de la enfermedad más allá del propio
-            diagnóstico.
-			Aquí encontraran información sobre alimentación, aspectos psicológicos,
-			avances médicos e investigaciones y entrenamientos para favorecer una mejor
-			calidad de vida.
+            diagnóstico. Aquí encontraran información sobre alimentación,
+            aspectos psicológicos, avances médicos e investigaciones y
+            entrenamientos para favorecer una mejor calidad de vida.
           </p>
         </figcaption>
       </figure>
