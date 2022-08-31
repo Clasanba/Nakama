04e376a8c9e66f930a2ab725e39f7840d8b59225
Clasanba <clasanba@gmail.com>
2022-08-31 18:16:49 +0000

    Complete database

--- src/api/models.py
+++ src/api/models.py
@@ -6,14 +6,85 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    first_name = db.Column(db.String(120), unique=False, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
+    user_name = db.Column(db.String(120), unique=False, nullable=False)
+    image = db.Column(db.String(120), unique=False, nullable=False)
+    favorite = db.relationship("Favorite",backref="User", lazy=True)
+    
     def __repr__(self):
-        return f'<User {self.email}>'
+        return '<User {self.email}>'
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "firt_name": self.first_name,
+            "last_name": self.last_name,
+            "user_name": self.user_name,
+            "image": self.image,
+        }
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    
+    def __repr__(self):
+        return '<Favorite {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+        }
+class Psychology(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image= db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+    url = db.Column(db.String(120), unique=False, nullable=False)
+    
+    def __repr__(self):
+        return '<Psychology {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "url": self.url,
+            "description": self.description,
+            "image": self.image,
+        }
+
+class Nutrition(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image= db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+    url = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Nutrition {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "url": self.url,
+            "description": self.description,
+            "image": self.image,
+        }
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image= db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+    url = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Article {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "url": self.url,
+            "description": self.description,
+            "image": self.image,
         }
