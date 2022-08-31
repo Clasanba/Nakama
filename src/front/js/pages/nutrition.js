571e44aa7b17ba3c7b27f2474c9f039141b3f0e0
Clasanba <clasanba@gmail.com>
2022-08-31 19:30:11 +0000

    Complete view nutrition and article

---
+++ src/front/js/pages/nutrition.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Cards from "../component/cards";
+
+const Nutrition = () => {
+  return (
+    <>
+      <h1>Nutrition</h1>
+      <Cards />
+    </>
+  );
+};
+
+export default Nutrition;
