28df93230790e46a1a1c5c25ea6228ac1e2f0bc4
Moyra_sc <noemisolac@gmail.com>
2022-09-13 17:16:06 +0000

    component profile completed

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import ProfileDate from "../component/profileDate";
 
@@ -9,3 +10,22 @@ const Profile = () => {
   );
 };
 export default Profile;
+=======
+import React from "react"
+import ProfileDate from "../component/profileDate"
+import ProfileUser from "../component/profileUser"
+
+const Profile = () =>{
+    return(
+    
+    <main className="container-fluid row">
+      <div className="col">
+      
+      <ProfileUser/>
+      </div>
+    </main>
+
+    )
+}
+export default Profile
+>>>>>>> 6f8473a (component profile completed)
