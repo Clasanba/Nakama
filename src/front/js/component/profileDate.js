5b6442af49e484d2ea29a82e9bda62b74071007e
Clasanba <clasanba@gmail.com>
2022-09-15 18:13:46 +0000

    modal delete in process

--- src/front/js/component/profileDate.js
+++ src/front/js/component/profileDate.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useContext, useEffect, useState } from "react";
 import "../../styles/index.css";
 import { Context } from "../store/appContext";
@@ -88,26 +87,6 @@ const ProfileDate = () => {
       </div>
       {showModal && <ModalDeleteUser />}
     </>
-=======
-import React from "react";
-import "../../styles/index.css"
-
-const ProfileDate = () => {
-  return (
-    <div className="card border-success  mt-3 d-flex justify-content-center" style={{ width: 30 + "rem", height: 17 + "rem" }}>
-      <div className="card-header text-center ">Nombre de usuario</div>
-      <div className="card-body">
-        <p className="card-text">Nombre:</p>
-        <p className="card-text">Primer apellido:</p>
-        <p className="card-text">Segundo apellido:</p>
-        <p className="card-text">Correo electrónico:</p>
-        <form className="d-flex justify-content-end mb-2">
-        <button className="btn btn-md btn btn-outline-danger " type="delete">Eliminar cuenta</button>
-        </form>          
-                
-      </div>
-    </div>
->>>>>>> 6f8473a (component profile completed)
   );
 };
 
