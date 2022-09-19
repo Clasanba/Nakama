10f88bf92ca8c384dcd82309f010833382ab71cf
Moyra_sc <noemisolac@gmail.com>
2022-09-19 21:06:00 +0000

    first desing home

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,7 +4,7 @@ import "../../styles/footer.css";
 export const Footer = () => {
   return (
     <>
-      <footer className="footer mt-auto py-3 text-center bg-success bg-gradient styleFoot ">
+      <footer className="footer  py-3 text-center bg-success  styleFoot ">
         <div className="container-fluid d-flex justify-content-center ">
           <a>
             <i className="fa-brands fa-square-facebook me-3 fs-2 text-white "></i>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import logoNakama from "../../img/Logo_Nakama-removebg.png"
+import logoNakama1 from "../../img/logo_corregido.png";
 import "../../styles/home.css";
 import { isLoggedIn } from "../auth";
 
@@ -8,16 +9,19 @@ const Home = () => {
   const hasLogin = isLoggedIn();
 
   return (
-    <div className="d-flex justify-content-center  ">
-      <figure className="card border-0" style={{ width: 40 + "rem" }}>
+    <div className="d-flex justify-content-center  body-home">
+      <figure className="card border-0  figure-body" style={{ width: 40 + "rem" }}>
+        <div className="img-container ">
         <img
-          src={logoNakama}
-          className="card-img-top logo_nakamaHome"
+          src={logoNakama1}
+          className="card-img-top logo_nakamaHome "
           alt="logo_nakama"
         />
-        {!hasLogin ? "No estás logueado!" : ""}
+        </div>
+        <p className="letters text-center">NAKAMA</p>
         <figcaption className="card-body">
-          <p className="card-text text-center">
+          <div className="description">
+          <p className="card-text text-center detail">
             Somos una comunidad dirigida a personas relacionadas directa o
             indirectamente con el cáncer, a través de la cual, puedan aprender
             aspectos sobre el día a día de la enfermedad más allá del propio
@@ -25,6 +29,7 @@ const Home = () => {
             aspectos psicológicos, avances médicos e investigaciones y
             entrenamientos para favorecer una mejor calidad de vida.
           </p>
+          </div>
         </figcaption>
       </figure>
     </div>
