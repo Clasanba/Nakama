50467ef6301eeea99d3e3b374e9a42dd31665ba5
Clasanba <clasanba@gmail.com>
2022-09-19 11:08:26 +0000

    Create view profile and modificate date profile

--- src/front/js/component/profileDate.js
+++ src/front/js/component/profileDate.js
@@ -1,28 +1,48 @@
 import React, { useContext, useEffect } from "react";
-import "../../styles/index.css"
+import "../../styles/index.css";
 import { Context } from "../store/appContext";
+import "../../styles/profileDate.css";
 
 const ProfileDate = () => {
-  const {store, actions} = useContext(Context)
+  const { store, actions } = useContext(Context);
 
   useEffect(() => {
     actions.getDataProfile();
   }, []);
 
   return (
-    <div className="card border-success  mt-3 d-flex justify-content-center" style={{ width: 30 + "rem", height: 17 + "rem" }}>
-      <div className="card-header text-center ">{store.user.user_name}</div>
-      <div className="card-body">
-        <p className="card-text"> Nombre: {store.user.name}</p>
-        <p className="card-text"> Primer apellido: {store.user.first_name}</p>
-        <p className="card-text"> Segundo apellido: {store.user.last_name}</p>
-        <p className="card-text"> Correo electrónico: {store.user.email}</p>
-        <form className="d-flex justify-content-end mb-2">
-        <button className="btn btn-md btn btn-outline-danger " type="delete">Eliminar cuenta</button>
-        </form>          
-                
+    <>
+      <div className="card profile-date">
+        <img
+          src={store.user.image}
+          className="avatar rounded-circle img-thumbnail img-profile-date "
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{store.user.user_name}</h5>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"> Nombre: {store.user.name}</li>
+          <li className="list-group-item">
+            Primer apellido: {store.user.first_name}
+          </li>
+          <li className="list-group-item">
+            Segundo apellido: {store.user.last_name}
+          </li>
+          <li className="list-group-item">
+            Correo electrónico: {store.user.email}
+          </li>
+        </ul>
+        <div className="card-body">
+          <button className="btn btn-md btn btn-outline-danger " type="delete">
+            Eliminar cuenta
+          </button>
+          <button className="btn btn-md btn btn-outline-danger " type="button">
+            Modificar datos de usuario
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
