62765008003ccf42609bcd27bbc09e274f1a53f0
Moyra_sc <noemisolac@gmail.com>
2022-09-19 21:40:18 +0000

    add letter Nakama

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -10,17 +10,18 @@ const Home = () => {
 
   return (
     <div className="d-flex justify-content-center  body-home">
-      <figure className="card border-0  figure-body" style={{ width: 40 + "rem" }}>
+      <figure className="card border-0  figure-body mt-3" style={{ width: 40 + "rem" }}>
+      
         <div className="img-container ">
         <img
           src={logoNakama1}
-          className="card-img-top logo_nakamaHome "
+          className="card-img-top logo_nakamaHome mb-2"
           alt="logo_nakama"
         />
         </div>
-        <p className="letters text-center">NAKAMA</p>
+        <p className="letters text-center m-0">NAKAMA</p>
         <figcaption className="card-body">
-          <div className="description">
+          <div className="description p-1">
           <p className="card-text text-center detail">
             Somos una comunidad dirigida a personas relacionadas directa o
             indirectamente con el cáncer, a través de la cual, puedan aprender
