d4098f55f5090a5be419153f2b2f121abee59337
Clasanba <clasanba@gmail.com>
2022-09-21 17:24:08 +0000

    added title and url to  database favorites class

--- src/api/routes.py
+++ src/api/routes.py
@@ -278,7 +278,9 @@ def add_favorite():
         return jsonify({}),400 
     
     url = request.json.get("url")
-    fav = Favorite(user_id = user.id, url = url)
+    url_image = request.json.get("url_image")
+    title = request.json.get("title")
+    fav = Favorite(user_id = user.id, url = url,url_image=url_image,title=title)
     
     db.session.add(fav)
     db.session.commit()
@@ -318,5 +320,5 @@ def read_favorites():
     def to_json(fav):
         return fav.serialize()
     
-    return jsonify(map(to_json, favorites)),200 
+    return jsonify(list(map(to_json, favorites))),200 
 
