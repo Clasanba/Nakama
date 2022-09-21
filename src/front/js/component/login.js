eda3d365cbbc536e0d826fdb8369123befc3338c
Moyra_sc <noemisolac@gmail.com>
2022-09-21 16:18:42 +0000

    fetch and modal completed

--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -117,10 +117,8 @@ export const Login = () => {
         <a href="#" data-bs-toggle="modal" data-bs-target="#modalRememberpass">
           <p>¿Olvidaste la contraseña?</p>
         </a>
-        <ModalRecoveryPass/>
-
+        <ModalRecoveryPass />
       </div>
-      
     </>
   );
 };
