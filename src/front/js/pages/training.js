11562fab09f4af881a42d3a297227b5f3172eceb
Clasanba <clasanba@gmail.com>
2022-09-22 16:31:02 +0000

    Active add favorite functionality

--- src/front/js/pages/training.js
+++ src/front/js/pages/training.js
@@ -3,6 +3,13 @@ import { CardTraining } from "../component/card_training";
 import { Favorites } from "../component/favorites";
 
 const Training = () => {
+  const [refresh, setRefresh] = useState(true);
+  const refreshFav = () => {
+    setRefresh(true);
+  };
+  const onRefresh = () => {
+    setRefresh(false);
+  };
   return (
     <>
       <div className=" card-group">
