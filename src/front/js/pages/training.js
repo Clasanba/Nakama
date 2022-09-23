a4ed98e7da5a955966879e0fa2d2e79ab0ed879f
Clasanba <clasanba@gmail.com>
2022-09-23 11:11:26 +0000

    fixed error delete user and error add favorites

--- src/front/js/pages/training.js
+++ src/front/js/pages/training.js
@@ -3,13 +3,6 @@ import { CardTraining } from "../component/card_training";
 import { Favorites } from "../component/favorites";
 
 const Training = () => {
-  const [refresh, setRefresh] = useState(true);
-  const refreshFav = () => {
-    setRefresh(true);
-  };
-  const onRefresh = () => {
-    setRefresh(false);
-  };
   return (
     <>
       <div className=" card-group">
