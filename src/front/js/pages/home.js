570c9dc6f6c262ada687def29dfdb7b5aa286dc9
Clasanba <clasanba@gmail.com>
2022-09-27 11:17:30 +0000

    testing things

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -9,32 +9,16 @@ const Home = () => {
   const hasLogin = isLoggedIn();
 
   return (
-    <div className="d-flex justify-content-center   body-home">
-      <figure
-        className="card border-0  figure-body mt-3"
-        style={{ width: 40 + "rem" }}
-      >
-        <div className="img-container ">
-          {/* <img
-          src={logoNakama1}
-          className="card-img-top logo_nakamaHome mb-2"
-          alt="logo_nakama"
-        /> */}
-        </div>
-        <p className="letters text-center m-0">NAKAMA</p>
-        <figcaption className="card-body">
-          <div className="description p-1">
-            <p className="card-text text-center detail">
-              Somos una comunidad dirigida a personas relacionadas directa o
-              indirectamente con el cáncer, a través de la cual, puedan aprender
-              aspectos sobre el día a día de la enfermedad más allá del propio
-              diagnóstico. Aquí encontraran información sobre alimentación,
-              aspectos psicológicos, avances médicos e investigaciones y
-              entrenamientos para favorecer una mejor calidad de vida.
-            </p>
-          </div>
-        </figcaption>
-      </figure>
+    <div className=" body-home bg-gradient  ">
+      <p className="letters text-light text-left m-0 ">NAKAMA</p>
+      <p className="card-text text-center detail">
+        Somos una comunidad dirigida a personas relacionadas directa o
+        indirectamente con el cáncer, a través de la cual, puedan aprender
+        aspectos sobre el día a día de la enfermedad más allá del propio
+        diagnóstico. Aquí encontraran información sobre alimentación, aspectos
+        psicológicos, avances médicos e investigaciones y entrenamientos para
+        favorecer una mejor calidad de vida.
+      </p>
     </div>
   );
 };
