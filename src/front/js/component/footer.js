9524e3f287e5f62cdf1277dca527a90c0ea3b702
Clasanba <clasanba@gmail.com>
2022-09-28 15:57:09 +0000

    location elements home

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,22 +4,22 @@ import "../../styles/footer.css";
 export const Footer = () => {
   return (
     <>
-      <footer className="footer  py-3 text-center   styleFoot ">
-        <div className="container-fluid d-flex justify-content-center ">
+      <footer className="  py-3 text-center  footer ">
+        <div className="item-footer ">
           <a
             href="https://www.facebook.com/profile.php?id=100085517563751"
             target="_blank"
           >
-            <i className="fa-brands fa-square-facebook me-3 fs-2  "></i>
+            <i className="fa-brands fa-square-facebook me-3 fs-2 text-white "></i>
           </a>
           <a href="https://www.instagram.com/comunidadnakama/" target="_blank">
-            <i className="fa-brands fa-instagram me-3 fs-2"></i>
+            <i className="fa-brands fa-instagram me-3 fs-2 text-white"></i>
           </a>
           <a href="https://twitter.com/Nakama_2022 " target="_blank">
-            <i className="fa-brands fa-twitter me-3 fs-2 "></i>
+            <i className="fa-brands fa-twitter me-3 fs-2 text-white"></i>
           </a>
           <a href="mailto:comunidadnakama@gmail.com" target="_blank">
-            <i className="fa-regular fa-envelope me-3 fs-2 "></i>
+            <i className="fa-regular fa-envelope me-3 fs-2 text-white"></i>
           </a>
         </div>
       </footer>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -10,15 +10,17 @@ const Home = () => {
 
   return (
     <div className=" body-home  ">
-      <p className="letters text-light text-left m-0 ">NAKAMA</p>
-      <p className="card-text text-center detail">
-        Somos una comunidad dirigida a personas relacionadas directa o
-        indirectamente con el cáncer, a través de la cual, puedan aprender
-        aspectos sobre el día a día de la enfermedad más allá del propio
-        diagnóstico. Aquí encontraran información sobre alimentación, aspectos
-        psicológicos, avances médicos e investigaciones y entrenamientos para
-        favorecer una mejor calidad de vida.
-      </p>
+      <p className="letters text-light text-left  align-middle ">NAKAMA</p>
+      <div className="paragraph">
+        <p className="card-text  detail text-light">
+          Somos una comunidad dirigida a personas relacionadas directa o
+          indirectamente con el cáncer, a través de la cual, puedan aprender
+          aspectos sobre el día a día de la enfermedad más allá del propio
+          diagnóstico. Aquí encontraran información sobre alimentación, aspectos
+          psicológicos, avances médicos e investigaciones y entrenamientos para
+          favorecer una mejor calidad de vida.
+        </p>
+      </div>
     </div>
   );
 };
