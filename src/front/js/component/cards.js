26bc934400aef366468f4743938fef01c746c199
Clasanba <clasanba@gmail.com>
2022-10-01 20:22:21 +0000

    complete locations cards library

--- src/front/js/component/cards.js
+++ src/front/js/component/cards.js
@@ -1,18 +1,21 @@
 import React from "react";
 import cards from "../../styles/cards.css";
+import { Link, useLocation } from "react-router-dom";
 
 const Cards = ({ articles }) => {
   return articles.map((item) => {
+    /*const location = useLocation();*/
     return (
       <article className="m-2" key={item.id}>
-        <div className="card h-100 ">
-          <img src={item.image} className="image-card card-img-top" />
+        <div className=" h-100 card-css">
+          <a href={item.url} target="_blank">
+            <img src={item.image} className=" image-card card-img-top" />
+          </a>
           <div className="card-body ">
-            <h5 className="card-title text-info">{item.title}</h5>
-            <p className="card-text">{item.description}</p>
-            <a href={item.url} target="_blank" className="btn btn-outline-info">
-              Leer más
+            <a href={item.url} target="_blank" className="text-decoration-none">
+              <h5 className="card-title text-info">{item.title}</h5>
             </a>
+            <p className="card-text">{item.description}</p>
           </div>
         </div>
       </article>
@@ -21,3 +24,10 @@ const Cards = ({ articles }) => {
 };
 
 export default Cards;
+/*{classNames(
+              {
+                "title-nutrition":{location.nutrition}, 
+                "title-psychology":{location.psychology},
+                "title-articles":{location.article},
+              
+              }*/
