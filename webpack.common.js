a9770893d0ee8ce938ff6b0ca85935c2ba7e4d70
Clasanba <clasanba@gmail.com>
2022-10-05 11:59:01 +0000

    install Webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,48 +1,61 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  entry: [
-    './src/front/js/index.js'
-  ],
+  entry: ["./src/front/js/index.js"],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/'
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+    publicPath: "/",
   },
   module: {
     rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          {
+            loader: "style-loader", // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+            options: {
+              url: false,
+            },
+          },
+        ],
+      }, //css only files
+      {
+        test: /\.(png|svg|jpg|gif|jpeg|webp)$/,
+        use: {
+          loader: "file-loader",
+          options: { name: "[name].[ext]" },
         },
-        {
-          test: /\.(css|scss)$/, use: [{
-              loader: "style-loader" // creates style nodes from JS strings
-          }, {
-              loader: "css-loader" // translates CSS into CommonJS
-          }]
-        }, //css only files
-        {
-          test: /\.(png|svg|jpg|gif|jpeg|webp)$/, use: {
-            loader: 'file-loader',
-            options: { name: '[name].[ext]' }
-          }
-        }, //for images
-        { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, use: ['file-loader'] } //for fonts
-    ]
+      }, //for images
+      {
+        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+        use: ["file-loader"],
+      }, //for fonts
+    ],
   },
   resolve: {
-    extensions: ['*', '.js']
+    extensions: ["*", ".js"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-        favicon: 'icono_nakama.ico',
-        template: 'template.html'
+      favicon: "icono_nakama.ico",
+      template: "template.html",
     }),
-    new Dotenv({ safe: true, systemvars: true })
-  ]
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/front/img", to: "images" }],
+    }),
+    new Dotenv({ safe: true, systemvars: true }),
+  ],
 };
