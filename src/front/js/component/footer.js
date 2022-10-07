6f2d9957ecff6dbcf06f33f4be2899090c193887
Moyra_sc <noemisolac@gmail.com>
2022-10-07 18:59:53 +0000

    add our names to footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,22 +4,25 @@ import "../../styles/footer.css";
 export const Footer = () => {
   return (
     <>
-      <footer className="  py-3 text-center  footer ">
+      <footer className="  py-3 text-center  footer container-fluid">
+      <div className="copy-name ">
+          <p className=" fw-bold  m-0 mt-2"> &copy; Claudia Sandubete & Noe Sola  </p>
+        </div>
         <div className="item-footer ">
           <a
             href="https://www.facebook.com/profile.php?id=100085517563751"
             target="_blank"
           >
-            <i className="fa-brands icons-footer fa-square-facebook me-3 fs-2 text-with-color "></i>
+            <i className="fa-brands icons-footer fa-square-facebook me-3 fs-3 text-with-color "></i>
           </a>
           <a href="https://www.instagram.com/comunidadnakama/" target="_blank">
-            <i className="fa-brands icons-footer fa-instagram me-3 fs-2 text-with-color"></i>
+            <i className="fa-brands icons-footer fa-instagram me-3 fs-3 text-with-color"></i>
           </a>
           <a href="https://twitter.com/Nakama_2022 " target="_blank">
-            <i className="fa-brands icons-footer fa-twitter me-3 fs-2 text-with-color"></i>
+            <i className="fa-brands icons-footer fa-twitter me-3 fs-3 text-with-color"></i>
           </a>
           <a href="mailto:comunidadnakama@gmail.com" target="_blank">
-            <i className="fa-regular icons-footer fa-envelope me-3 fs-2 text-with-color"></i>
+            <i className="fa-regular icons-footer fa-envelope me-3 fs-3 text-with-color"></i>
           </a>
         </div>
       </footer>
